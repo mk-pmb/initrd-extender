ffe2f1e4c7ebfb754b058a4524e587842d3621dc
mk-pmb <mk-pmb@users.noreply.github.com>
2026-06-24 21:14:13 +0200

    [docs] serialport-init: Explain why it's not in net-util-pmb.

--- util/bin-all/serialport-init.sh
+++ util/bin-all/serialport-init.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # -*- coding: utf-8, tab-width: 2 -*-
+#
+#   NB: In net-util-pmb, `serialport-repl.sh` already has `stty` config.
+#   NB: In net-util-pmb, `serialport-socat.sh` has `--prepare-only`.
+#
 stty -F /dev/ttyUSB0 115200 sane -cstopb -echo raw litout -echo
